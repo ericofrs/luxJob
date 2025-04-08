1e0c91486a395263ea0b04612694196891a49e92
ericofrs <ericoqv@gmail.com>
2025-04-08 12:35:35 +0200

    first phase of package creation is done. Now we need to create all the functions

---
+++ R/book_recommendations.R
@@ -0,0 +1,16 @@
+#' Get Book Recommendations
+#'
+#' Returns a list of recommended books. You can filter them by skill.
+#'
+#' @param param1
+#'
+#' @returns A data frame with columns 'book_id', 'title', 'author', and 'skill_id'.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' getbooks()
+#' }
+get_book <- function(param1){
+
+}
